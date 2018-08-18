e580955e20092cd3af6b8feddfbbf6f4ecc397b9
robin <zhangbinatp@gmail.com>
2018-08-18 10:59:03 +0800

    download_script_tflite_model
    Signed-off-by: robin <zhangbinatp@gmail.com>

---
+++ TfLiteCameraDemo/app/src/main/assets/download.sh
@@ -0,0 +1,4 @@
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/conv_actions_tflite.zip
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/mobilenet_ssd_tflite_v1.zip
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/mobilenet_v1_224_android_quant_2017_11_08.zip
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
